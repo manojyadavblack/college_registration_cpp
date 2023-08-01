40a113676c5b8273587bd0c4bd55b55abf7f7bd5
manojyadavblack <88652936+manojyadavblack@users.noreply.github.com>
2023-08-01 13:09:48 +0530

    Add files via upload
    https://www.cppbuzz.com/projects/c++/c++-project-on-college-registration-system 
    this is the  original source

---
+++ temp.cpp
@@ -0,0 +1,24 @@
+// C program for splitting a string
+// using strtok()
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "Geeks-for-Geeks";
+
+    // Returns first token
+    char *token = strtok(str, " - ");
+
+    // Keep printing tokens while one of the
+    // delimiters present in str[].
+    token = strtok(NULL, " - ");
+    while (token != NULL)
+    {
+        printf(" % s\n", token);
+        token = strtok(NULL, " - ");
+        // here we pass NULL , bcz we want to work on original string only.
+    }
+
+    return 0;
+}
